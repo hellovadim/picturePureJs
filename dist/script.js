0538870a085dcc27cc4239aff83347bcc86c3af6
Vadim <hellovados@gmail.com>
2021-07-06 21:26:09 +0300

    добавил модальные окна и при помощи getComputedStyle проверили открыты ли другие окна

--- dist/script.js
+++ dist/script.js
@@ -959,52 +959,57 @@ var modals = function modals() {
     var trigger = document.querySelectorAll(triggerSelector),
         modal = document.querySelector(modalSelector),
         close = document.querySelector(closeSelector),
-        windows = document.querySelectorAll('[data-modal]');
+        windows = document.querySelectorAll("[data-modal]");
     trigger.forEach(function (item) {
-      item.addEventListener('click', function (e) {
+      item.addEventListener("click", function (e) {
         if (e.target) {
           e.preventDefault();
         }
 
         windows.forEach(function (item) {
-          item.style.display = 'none';
+          item.style.display = "none";
         });
         modal.style.display = "block";
-        document.body.style.overflow = "hidden"; // document.body.classList.add('modal-open')
+        document.body.style.overflow = "hidden";
       });
     });
-    close.addEventListener('click', function () {
+    close.addEventListener("click", function () {
       modal.style.display = "none";
-      document.body.style.overflow = ""; // document.body.classList.remove('modal-open')
-
+      document.body.style.overflow = "";
       windows.forEach(function (item) {
-        item.style.display = 'none';
+        item.style.display = "none";
       });
     });
-    modal.addEventListener('click', function (e) {
+    modal.addEventListener("click", function (e) {
       if (e.target === modal && closeClickOverlay) {
         windows.forEach(function (item) {
-          item.style.display = 'none';
+          item.style.display = "none";
         });
         modal.style.display = "none";
-        document.body.style.overflow = ""; // document.body.classList.remove('modal-open')
+        document.body.style.overflow = "";
       }
     });
   }
 
   function showModalByTime(selector, time) {
     setTimeout(function () {
-      document.querySelector(selector).style.display = 'block';
-      document.body.style.overflow = 'hidden';
+      var display;
+      document.querySelectorAll("[data-modal]").forEach(function (item) {
+        if (getComputedStyle(item).display !== "none") {
+          display = "block";
+        }
+      });
+
+      if (!display) {
+        document.querySelector(selector).style.display = "block";
+        document.body.style.overflow = "hidden";
+      }
     }, time);
   }
 
-  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-  bindModal('.phone_link', '.popup', '.popup .popup_close');
-  showModalByTime('.popup', 60000);
-  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
-  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
-  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+  bindModal(".button-design", ".popup-design", ".popup-design .popup-close");
+  bindModal(".button-consultation", ".popup-consultation", ".popup-consultation .popup-close");
+  showModalByTime('.popup-consultation', 5000);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modals);
